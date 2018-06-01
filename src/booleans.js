7ef15790f961251f9d0923bee5bfb8ebbe2cfd94
Carrie Dennett <carriedennett2016@gmail.com>
2018-06-01 20:18:13 +0100

    boolean problems solved part a

--- src/booleans.js
+++ src/booleans.js
@@ -17,35 +17,55 @@ const none = (a, b) => {
 };
 
 const one = (a, b) => {
-  // your code here
+  if(a === true ^ b === true ) {
+    return true
+  }
+  else {
+    return false
+  }
 };
 
 const truthiness = (a) => {
-  // your code here
+  if (a) {
+    return true
+  }
+  else {
+    return false
+  }
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return a === b
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return a > b
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return a <= b
 };
 
 const isOdd = (a) => {
-  // your code here
+ if (a % 2 === 0) {
+   return false
+ }
+ else {
+   return true
+ }
 };
 
 const isEven = (a) => {
-  // your code here
+  if (a % 2 === 0) {
+    return true
+  }
+  else {
+    return false
+  }
 };
 
 const isSquare = (a) => {
-  // your code here
+  return Math.sqrt(a) % 1 === 0
 };
 
 const startsWith = (char, string) => {
